346e0d9e25382ef72d0504d8457eed3aed8576c3
jzhfighting <312456051@qq.com>
2015-09-14 16:50:37 +0800

    在将图片移动时出现问题

--- game1/Form1.Designer.cs
+++ game1/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 276);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Draw);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 331);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- game1/Form1.cs
+++ game1/Form1.cs
@@ -12,9 +12,45 @@ namespace game1
 {
     public partial class Form1 : Form
     {
+        private int x = 50, y = 50;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap r=new Bitmap("role.png");
+            r.SetResolution(96,96);
+            g.RotateTransform(30);
+            g.DrawImage(r,0,0);
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(@"role.png");
+            bitmap.SetResolution(96, 96);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawImage(bitmap, x, y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            DialogResult r1 = MessageBox.Show("有按键按下");
+            if (e.KeyCode == Keys.Up){    y = y - 5; }
+            if (e.KeyCode == Keys.Down) { y = y + 5; }
+            if (e.KeyCode == Keys.Left) { x = x - 5; }
+            if (e.KeyCode == Keys.Right) { x = x + 5; }
+            Draw(sender,e);
+        }
+        private void Draw1()
+        {
+            Bitmap bitmap = new Bitmap(@"role.png");
+            bitmap.SetResolution(96, 96);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawImage(bitmap, x, y);
         }
     }
 }
